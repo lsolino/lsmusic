c4b16c3903a3c5683930305cdcd79df4a2569034
Luan Solino <luansolino@gmail.com>
2020-04-18 17:28:08 -0300

    endpoint's searches created

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :dashboards, only: :index
+      resources :searches, only: :index
+      resources :categories, only: :index
     end
   end
 end
