4f5d4544c4dd101fcf00fbb5920446cccaf137c6
Luan Solino <luansolino@gmail.com>
2020-04-18 19:50:11 -0300

    bulma included in project

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
- 
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
+
 const App = () => {
   return(
     <Fragment>
-      Hello
+      <Button color="primary">Testando o Bulma</Button>
     </Fragment>
   );
 }
-
 export default App;
