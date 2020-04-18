9e8bbc495f7b1daef4f745f07c2095f935dd5b0d
Luan Solino <luansolino@gmail.com>
2020-04-18 19:54:45 -0300

    react routes included

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Button } from 'react-bulma-components';
-
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom'
+ 
 const App = () => {
   return(
     <Fragment>
-      <Button color="primary">Testando o Bulma</Button>
+      <BrowserRouter>
+        <Routes/>
+      </BrowserRouter>
     </Fragment>
   );
 }
+
 export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,6 @@ Rails.application.routes.draw do
       end  
     end
   end
+
+  get "*path", to: "home#index", :constraints => lambda{|req| req.path !~ /\.(png|jpg|js|css|json)$/ }
 end
