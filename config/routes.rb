effa5ba649e7e2138b87c86d8717fd55637837e9
Luan Solino <luansolino@gmail.com>
2020-04-18 18:20:48 -0300

    endpoint's favorites created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: 'home#index'
 
+  concern :favoritable do |options|
+    shallow do
+      post "/favorite", { to: "favorites#create", on: :member }.merge(options)
+      delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :dashboards, only: :index
@@ -10,7 +17,15 @@ Rails.application.routes.draw do
       resources :categories, only: [:index, :show]
       resources :albums, only: :show do
         resources :recently_heards, only: :create
+        concerns :favoritable, favoritable_type: 'Album'
       end
+      resources :favorites, only: :index
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end  
+      resources :artists, only: [] do
+        concerns :favoritable, favoritable_type: 'Artist'
+      end  
     end
   end
 end
