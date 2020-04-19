ff7a466cacf803b61be8372083d98a2269eb1116
Luan Solino <luansolino@gmail.com>
2020-04-19 19:36:24 -0300

    Favorites included

--- app/javascript/components/musics/music/index.js
+++ app/javascript/components/musics/music/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Heading, Columns } from 'react-bulma-components';
 import styled from 'styled-components'
 import { FaPlayCircle, FaStopCircle } from "react-icons/fa";
+import Favorite from '../../common/favorite';
 
 const MusicSeparator = styled.hr`
  height: 1px;
@@ -38,7 +39,7 @@ const Music = (props) => {
               </CustomSubHeading>
             </Columns.Column>
             <Columns.Column desktop={{ size: 4 }} mobile={{ size: 4 }} className='is-pulled-right has-text-right'>
-              {/* Favorite */}
+              <Favorite id={props.song.id} kind='songs' favored={props.song.favorite} />
             </Columns.Column>
           </Columns>
           <MusicSeparator />
--- app/views/api/v1/albums/show.json.jbuilder
+++ app/views/api/v1/albums/show.json.jbuilder
@@ -8,4 +8,5 @@ json.songs @album.songs.each do |song|
   json.file_url url_for(song.file)
   json.artist_name @album.artist.name
   json.album_id song.album.id
+  json.favorite current_user.is_favorite? 'Song', song.id
 end
