fb7be949e0ad932c989c401e01c60d3109bfe12f
Luan Solino <luansolino@gmail.com>
2020-04-18 21:04:15 -0300

    menu bar created

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom'
- 
+import Menu from './components/common/menu/index'
+
 const App = () => {
   return(
     <Fragment>
       <BrowserRouter>
+        <Menu/>
         <Routes/>
       </BrowserRouter>
     </Fragment>
