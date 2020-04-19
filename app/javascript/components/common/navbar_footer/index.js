3290819ee3dd980b9218dd4892dddf486dcaaea4
Luan Solino <luansolino@gmail.com>
2020-04-18 23:04:17 -0300

    discovery component created

--- app/javascript/components/common/navbar_footer/index.js
+++ app/javascript/components/common/navbar_footer/index.js
@@ -44,4 +44,5 @@ const NavbarFooter = () => {
     </Fragment>
   );
 }
+
 export default NavbarFooter;
--- app/javascript/screens/discovery/index.js
+++ app/javascript/screens/discovery/index.js
@@ -1,9 +1,14 @@
-import React, { Fragment, useState } from 'react';
+import React, {Fragment} from 'react';
+import SectionWrapper from '../../components/common/section_wrapper'
+import Discovery from '../../components/discovery';
 import NavbarFooter from '../../components/common/navbar_footer';
 
 const DiscoveryScreen = () => {
-  return (
+  return(
     <Fragment>
+      <SectionWrapper>
+        <Discovery />
+      </SectionWrapper>
       <NavbarFooter />
     </Fragment>
   );
