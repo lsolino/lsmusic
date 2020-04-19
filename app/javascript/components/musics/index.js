d384d8f487b8e21aae56b89e0a61b590f3c9aa26
Luan Solino <luansolino@gmail.com>
2020-04-19 18:06:35 -0300

    playing music in music player

--- app/javascript/components/musics/index.js
+++ app/javascript/components/musics/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import Music from './music';
 import { Button, Columns } from 'react-bulma-components';
@@ -11,6 +11,40 @@ const Musics = (props) => {
 
   const [songs, setSongs] = useState([]);
   const [playing, setPlaying] = useState([]);
+  const AudioRef = useRef();
+
+  const [playRandom, setPlayRandom] = useState(false);
+
+  const NextSong = () => {
+    if (playRandom) {
+      let index = Math.floor(Math.random() * props.songs.length);
+      setPlaying(props.songs[index]);
+    } else
+      setPlaying([]);
+  }
+
+  const SwitchRandom = () => {
+    if (playRandom) {
+      setPlayRandom(false);
+      setPlaying([]);
+    } else
+      setPlayRandom(true);
+  }
+
+  useEffect(() => {
+    if (playRandom)
+      NextSong();
+  }, [playRandom]);
+
+  useEffect(() => {
+    if (AudioRef.current !== null) {
+      AudioRef.current.pause();
+      AudioRef.current.load();
+      if (playing.id) {
+        AudioRef.current.play();
+      }
+    }
+  }, [playing]);
 
   useEffect(() => {
     setSongs(props.songs.map((song, key) =>
@@ -32,9 +66,14 @@ const Musics = (props) => {
           <PlaySequenceButton
             className='is-medium'
             color='primary'
-            outlined>
-            Tocar aleatório
+            outlined
+            onClick={() => SwitchRandom()}
+          >
+            {playRandom == true ? 'Parar de tocar' : 'Tocar aleatoriamente'}
           </PlaySequenceButton>
+          <audio controls ref={AudioRef} onEnded={() => NextSong()} className='is-hidden'>
+            <source src={playing.file_url} />
+          </audio>
         </Columns.Column>
       </Columns>
       {songs}
